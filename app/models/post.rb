e4ea701156f9692610bf5c965f26ddec4d1736e0
Bunlong <bunlong.van@gmail.com>
2014-03-19 09:48:17 +0700

    testing post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
-  
+  validates :title, presence: true, length: { minimum: 5  }
+  validates :content, presence: true, length: { minimum: 10  }
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Post do
-  
+  it { should validate_presence_of(:title) }
+  it { should ensure_length_of(:title).is_at_least(5) }
+  it { should validate_presence_of(:content) }
+  it { should ensure_length_of(:content).is_at_least(10) }
 end
