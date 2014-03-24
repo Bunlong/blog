f32278e166911e4fbafd165f0b511b60039a40ce
Bunlong <bunlong.van@gmail.com>
2014-03-24 09:04:27 +0700

    column specification with Categorization

--- spec/models/categorization_spec.rb
+++ spec/models/categorization_spec.rb
@@ -9,3 +9,8 @@ describe Categorization, 'association' do
   it { should belong_to(:category) }
   it { should belong_to(:post) }
 end
+
+describe Categorization, 'column_specification' do
+  it { should have_db_column(:category_id).of_type(:integer).with_options(presence: true) }
+  it { should have_db_column(:post_id).of_type(:integer).with_options(presence: true) }
+end
